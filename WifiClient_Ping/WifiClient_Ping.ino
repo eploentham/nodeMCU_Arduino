c8c660e9aa2a4d75686966789a149f68e1337eeb
eploentham <eploentham@gmail.com>
2015-10-14 18:57:28 +0700

    581014
    bangna5

---
+++ WifiClient_Ping/WifiClient_Ping.ino
@@ -0,0 +1,121 @@
+/*
+HC-SR04 Ping distance sensor]
+VCC to arduino 5v GND to arduino GND
+Echo to Arduino pin 13 Trig to Arduino pin 12
+Red POS to Arduino pin 11
+Green POS to Arduino pin 10
+560 ohm resistor to both LED NEG and GRD power rail
+More info at: http://goo.gl/kJ8Gl
+Original code improvements to the Ping sketch sourced from Trollmaker.com
+Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+*/
+#include <ESP8266WiFi.h>
+const int trigPin =13;
+const int echoPin =12;
+const int led =11;
+const int led2= 10;
+
+
+const char* ssid     = "Bangna5_floor4_01";
+const char* password = "bng5linux";
+
+const char* host = "172.25.4.69";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
+
+void setup() {
+  Serial.begin (115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  //pinMode(led, OUTPUT);
+  //pinMode(led2, OUTPUT);
+
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+}
+int value = 0;
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 4) {  // This is where the LED On/Off happens
+    //digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+  //digitalWrite(led2,LOW);
+}
+  else {
+    //digitalWrite(led,LOW);
+    //digitalWrite(led2,HIGH);
+  }
+  if (distance >= 300 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  //delay(500);
+
+
+
+  delay(1000);
+  ++value;
+
+  //Serial.print("connecting to ");
+  //Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/cgi-bin/insert_temp4.py?devi=10002&devt=11&dist="+String(distance);
+
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  delay(10);
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  //Serial.println();
+  //Serial.println("closing connection");
+  
+}
